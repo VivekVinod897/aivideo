d555d013b724aa9563e55ce5d19793096d64ca86
VivekVinod897 <76860481+VivekVinod897@users.noreply.github.com>
2021-06-07 14:34:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+video = "";
+
+function preload(){
+video = createVideo('video.mp4');
+video.hide();
+}
+
+function setup(){
+canvas = createCanvas(480 , 380);
+canvas.center();
+}
+
+function start(){
+objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+}
+
+function draw(){
+image(video , 0 ,0 , 480 , 380);
+}
+
+function modelLoaded(){
+console.log("Model Is Loaded");
+video.loop();
+video.speed(1);
+video.volume(0);
+}
